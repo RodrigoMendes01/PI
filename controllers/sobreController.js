477d8870adbc956242ab4ab026e9b048e5bd0acd
beatrizvilar <beatrizvguima@gmail.com>
2022-07-12 16:58:06 -0300

    criando e adicionando itens no ProdutosModel.js

--- controllers/sobreController.js
+++ controllers/sobreController.js
@@ -4,4 +4,5 @@ const SobreController = {
   }
 };
 
-module.exports = SobreController
+module.exports = SobreController;
+
---
+++ models/ProdutosModel.js
@@ -0,0 +1,277 @@
+module.exports = {
+  produtos: [ 
+    // LIMPEZA
+    {
+    id: 1,
+    nome: "Água Sanitária Dragão 1L",
+    preco: "R$1,49",
+    imagem: "601_1.jpeg",
+    descricao: {}
+    },
+    {
+      id: 2,
+      nome: "Detergente Neutro Ipê 500ml",
+      preco: "R$1,89",
+      imagem: "detergente.webp",
+      descricao: {}
+    },
+    {
+      id: 3,
+      nome: "Desengordurante Veja 500ml",
+      preco: "R$7,10",
+      imagem: "desengordurante-cozinha.webp",
+      descricao: {}
+    },
+    {
+      id: 4,
+      nome: "Álcool Santa Cruz 1L",
+      preco: "R$6,20",
+      imagem: "alcool-santacruz.webp",
+      descricao: {}
+    },
+    {
+      id: 5,
+      nome: "Esponja Multi-Uso Scotch",
+      preco: "R$5,99",
+      imagem: "esponja-multiuso-Scotch.webp",
+      descricao: {}
+    },
+    {
+      id: 6,
+      nome: "Limpa Vidros Veja 500ml",
+      preco: "R$5,60",
+      imagem: "limpa-vidros-veja.webp",
+      descricao: {}
+    },
+    {
+      id: 7,
+      nome: "Limpador Multi-Uso Veja",
+      preco: "R$4,90",
+      imagem: "limpador-multiuso.jpeg",
+      descricao: {}
+    },
+    {
+      id: 8,
+      nome: "Desinfetante Omo 1L",
+      preco: "R$9,99",
+      imagem: "omo-desinfetante.webp",
+      descricao: {}
+    },
+    {
+      id: 9,
+      nome: "Sabão Neutro Ipê 1kg",
+      preco: "R$15,70",
+      imagem: "sabao-neutro-ipe.webp",
+      descricao: {}
+    },
+    {
+      id: 10,
+      nome: "Luvas de Borracha Sanro Soft",
+      preco: "R$6,90",
+      imagem: "luvas-sanro-soft.jpeg",
+      descricao: {}
+    },
+    {
+      id: 11,
+      nome: "Pano Multi-Uso Scotch",
+      preco: "R$16,60",
+      imagem: "pano-multiuso-scotch.avif",
+      descricao: {}
+    },
+    {
+      id: 12,
+      nome: "Sabão Líquido Ariel 1,2L",
+      preco: "R$19,99",
+      imagem: "sabao-liquido-ariel.webp",
+      descricao: {}
+    },
+    {
+      id: 13,
+      nome: "Amaciante Comfort 1L",
+      preco: "R$14,75",
+      imagem: "amaciante.webp",
+      descricao: {}
+    },
+    {
+      id: 14,
+      nome: "Sabão em Pó Omo 800g",
+      preco: "R$14,50",
+      imagem: "sabao-po.webp",
+      descricao: {}
+    },
+    {
+      id: 15,
+      nome: "Sapólio 250ml",
+      preco: "R$7,15",
+      imagem: "sapolio.jpeg",
+      descricao: {}
+    },
+
+    // HIGIENE
+    
+    {
+      id: 16,
+      nome: "Escova Colgate",
+      preco: "R$15,00",
+      imagem: "escova-colgate.jpeg",
+      descricao: {}
+    },
+    {
+      id: 17,
+      nome: "Condionador Pantene",
+      preco: "R$7,60",
+      imagem: "pantene-condicionador.jpeg",
+      descricao: {}
+    },
+    {
+      id: 18,
+      nome: "Protex Sabonete Mãos",
+      preco: "R$6,99",
+      imagem: "protex-maos.jpeg",
+      descricao: {}
+    },
+    {
+      id: 19,
+      nome: "Sabonete Dove Em Barra",
+      preco: "R$3,50",
+      imagem: "sabonete-dove.jpeg",
+      descricao: {}
+    },
+    {
+      id: 20,
+      nome: "Shampoo Pantene",
+      preco: "R$7,70",
+      imagem: "shampoo-pantene.webp",
+      descricao: {}
+    },
+    {
+      id: 21,
+      nome: "Pasta de Dente Colgate",
+      preco: "R$8,90",
+      imagem: "pasta-colgate.jpeg",
+      descricao: {}
+    },
+    {
+      id: 22,
+      nome: "Papel Higiênico Neve",
+      preco: "R$20,00",
+      imagem: "papel-higienico.jpeg",
+      descricao: {}
+    },
+    {
+      id: 23,
+      nome: "Desodorante Rexona Power Dry",
+      preco: "R$10,00",
+      imagem: "desodorante.webp",
+      descricao: {}
+    },
+    {
+      id: 24,
+      nome: "Desodorante Rexona Men",
+      preco: "R$10,00",
+      imagem: "desodorante-masc.webp",
+      descricao: {}
+    },
+   
+    // BEBIDAS 
+   
+    {
+      id: 25,
+      nome: "Água Mineral Crystal 500ml",
+      preco: "R$1,49",
+      imagem: "agua-crystal.webp",
+      descricao: {}
+    },
+    {
+      id: 26,
+      nome: "Água com Gás Crystal 500ml",
+      preco: "R$1,69",
+      imagem: "agua-gas-crystal.webp",
+      descricao: {}
+    },
+    {
+      id: 27,
+      nome: "Água Mineral Crystal 1,5L",
+      preco: "R$2,50",
+      imagem: "agua-1.5l.webp",
+      descricao: {}
+    },
+    {
+      id: 28,
+      nome: "Coca-Cola 350ml",
+      preco: "R$2,20",
+      imagem: "coca-lata.png",
+      descricao: {}
+    },
+    {
+      id: 29,
+      nome: "Guaraná 350ml",
+      preco: "R$1,99",
+      imagem: "guaraná-350.jpeg",
+      descricao: {}
+    },
+    {
+      id: 30,
+      nome: "Cerveja Heineken 350ml",
+      preco: "R$3,80",
+      imagem: "heineken.jpeg",
+      descricao: {}
+    },
+    {
+      id: 31,
+      nome: "Cerveja Devassa 350ml",
+      preco: "R$2,70",
+      imagem: "devassa.webp",
+      descricao: {}
+    },
+    {
+      id: 32,
+      nome: "Cerveja Skol Pilsen 350ml",
+      preco: "R$3,10",
+      imagem: "skol.wepb",
+      descricao: {}
+    },
+    {
+      id: 33,
+      nome: "Suco de Uva DelValle 1L",
+      preco: "R$7,20",
+      imagem: "uva-delvalle.jpeg",
+      descricao: {}
+    },
+    {
+      id: 34,
+      nome: "Suco de Laranja DelValle 1L",
+      preco: "R$7,10",
+      imagem: "laranja-delvalle.jpeg",
+      descricao: {}
+    },
+    {
+      id: 35,
+      nome: "Vinho Tinto Seco Chileno ",
+      preco: "R$31,00",
+      imagem: "vinho-tinto-seco.webp",
+      descricao: {}
+    },
+    {
+      id: 36,
+      nome: "Vinho Verde Casal Garcia",
+      preco: "R$42,00",
+      imagem: "casal-garcia.png",
+      descricao: {}
+    },
+    {
+      id: 37,
+      nome: "Vinho Branco Monte da Serra",
+      preco: "R$39,90",
+      imagem: "vinho-branco.webp",
+      descricao: {}
+    },
+    {
+      id: 38,
+      nome: "Vinho Tinto Casa di Luigi",
+      preco: "R$43,00",
+      imagem: "vinho-tinto-kosher.webp",
+      descricao: {}
+    },
+  ]
+}
