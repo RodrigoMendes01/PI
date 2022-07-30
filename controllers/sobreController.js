c545aed89f7d6a03f7e9ca75de7c5cf0c56331cc
Rodrigo <rodrigo.rm123.rm@gmail.com>
2022-07-29 22:08:09 -0300

    removendo arquivos estaticos da página home

--- controllers/sobreController.js
+++
@@ -1,7 +0,0 @@
-const sobreController = {
-  index: (req, res) => {
-    res.render('paginaSobre')
-  }
-};
-
-module.exports = sobreController
--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,13 @@
+
 const express = require("express");
 const router = express.Router();
+
+
 const homeController = require('../controllers/homeController');
-const sobreController = require("../controllers/sobreController");
-const cadastroController = require('../controllers/cadastroController')
+
 
 router.get("/", homeController.index);
 
-router.get("/sobre", sobreController.index)
-
-router.get("/cadastrar", cadastroController.index )
+router.get("/sobre", homeController.about)
 
 module.exports = router;
