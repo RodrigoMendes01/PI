61777104f9c20be426ce48dacddc8d15d9667863
Rodrigo <rodrigo.rm123.rm@gmail.com>
2022-08-02 17:47:44 -0300

    adicionando delete do CRUD

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,9 +3,10 @@ const Produto = require("../models/Produto")
 const homeController = {
   index: (req, res) => {
     const produtos = Produto.findAll()
+
     const produtosEmOferta = produtos.filter(produto => {
       return produto.desconto !== true && produto.desconto >= 5}).slice(0, 12)
-
+    
     const produtosUltimosVistos = produtos.filter(produto => produto.status === 'normal').slice(0, 12)
 
     res.render('paginaHome', {produtosEmOferta, produtosUltimosVistos})
--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -1,23 +1,27 @@
 /*** Require's ***/
 const fs = require('fs');
 const path = require('path');
-const Produto = require("../models/Produto")
+const Produto = require("../models/Produto");
 
+
+/****************************************************************************************/
 const produtoController = {
   /*** Get All Products ***/
+
   showAllProducts: (req, res) => {
     const produtos = Produto.findAll();
     res.render('paginaProdutos', {produtos})
   },
   /*** Creat One Product ***/
+
   createOneProduct: (req, res) => {
     res.render('cadastrarProduto')
   },
   /*** Store the Product ***/
+
   storeProduct: (req, res) => {
     const produtos = Produto.findAll();
     const produtoNovo = req.body
-    console.log(produtoNovo)
     const arquivoImagem = req.file.filename
     let newId = produtos.length + 1
 
@@ -36,13 +40,15 @@ const produtoController = {
       },
       categoria: produtoNovo.categoria  
     });
-    fs.writeFileSync(path.join(__dirname, '../data/produtos.json'), JSON.stringify(produtos));
+    Produto.save(produtos);
 
     res.redirect('/produtos/detalhe/' + newId)
   },
   /*** Show the detail of the product ***/
+
   detail: (req, res) => {
-    const produto = Produto.findOne(req.params.id)
+    const {id} = req.params
+    const produto = Produto.findOne(id)
 
     let produtosRelacionados = Produto.filter(produto.categoria)
     
@@ -55,33 +61,24 @@ const produtoController = {
   /*** Show the product that was choosed to edit  ***/
   edit: (req, res) => {
     const {id} = req.params 
+
     const produto = Produto.findOne(id)
     res.render('editarProduto', {produto})
   },
+  /*** Update the product that was choosed to edit ***/
   update: (req, res) => {
-    const produtos = Produto.findAll();
     const {id} = req.params
     const produtoRecebido = req.body
-    const imagemRecebida = req.file
 
-    const produtoQueSeraAtualizado = produtos.find(produto => produto.id == id)
-
-    const produtoAtualizado =
-    produtoQueSeraAtualizado.nome = produtoRecebido.nome
-    produtoQueSeraAtualizado.preco = produtoRecebido.preco
-    produtoQueSeraAtualizado.desconto = produtoRecebido.desconto
-    produtoQueSeraAtualizado.status = produtoRecebido.status
-    produtoQueSeraAtualizado.imagem = imagemRecebida
-    produtoQueSeraAtualizado.descricao.marca = produtoRecebido.marca
-    produtoQueSeraAtualizado.descricao.altura = produtoRecebido.altura
-    produtoQueSeraAtualizado.descricao.largura = produtoRecebido.largura
-    produtoQueSeraAtualizado.descricao.peso = produtoRecebido.peso
-    produtoQueSeraAtualizado.categoria = produtoRecebido.categoria
-
-    fs.writeFileSync(path.join(__dirname, '../data/produtos.json'), JSON.stringify(produtos));
-
+    Produto.update(id, produtoRecebido);
     res.redirect('/produtos/detalhe/' + id)
+  },
+  /**** Delete one product *********/
+  delete: (req, res) => {
+    const {id} = req.params
     
+    Produto.delete(id)
+    res.redirect('/produtos')
   }
 }
 
